22f0c5f0ab424948cfc3042624d643401dfc5915
ajifurai <furaiaji79@gmail.com>
2021-07-09 03:27:53 +0000

    ユーザ認証まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
     root to: 'tasks#index'
-    
     resources :tasks
+    
+    get 'signup', to: 'users#new'
+    resources :users, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
